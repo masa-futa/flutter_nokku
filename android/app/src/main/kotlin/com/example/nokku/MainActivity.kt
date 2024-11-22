df6a6f1312edf58e8bccb65b28ec97eb401a105f
masa-futa <masakunn1215@gmail.com>
2024-11-22 23:47:44 +0900

    [change] melos導入

--- android/app/src/main/kotlin/com/example/nokku/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.nokku
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
---
+++ packages/app/android/app/src/main/kotlin/com/example/nokku/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nokku
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
