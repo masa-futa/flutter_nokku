96931a353d350793e8589d9b378038b6d8806d34
masa-futa <masakunn1215@gmail.com>
2025-01-01 23:14:58 +0900

    update: theme project

---
+++ packages/sample_auto_route/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/masakifutami/fvm/versions/3.26.0-0.1.pre@beta"
+export "FLUTTER_APPLICATION_PATH=/Users/masakifutami/Documents/00_開発/02_勉強/01_flutter/01_nokku/packages/sample_auto_route"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
